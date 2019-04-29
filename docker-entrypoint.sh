401eb4ab0c3f195aaa365dc5d156f4ecb547ddd2
Dick Snel <d.snel@onvio.nl>
2019-04-29 11:31:36 +0200

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-/usr/share/dependency-check/bin/dependency-check.sh --scan "/tmp/src" --data "/tmp/dependency-check/data" --format "ALL"  --project ${PROJECT_NAME} $@
+/usr/share/dependency-check/bin/dependency-check.sh --scan "/tmp/src" --data "/tmp/dependency-check/data" --out "/tmp/reports" --format "ALL"  --project ${PROJECT_NAME} $@
